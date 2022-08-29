ae337421a97c492789106df15b3e0e7a07f5d827
KarlP17123 <karl.park@telia.com>
2022-08-29 18:39:47 +0000

    Add guess function, quiz progress and show score

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,56 @@ class Question {
     }
 
 }
+
+// DISPLAY QUESTION
+function displayQuestion() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+       // show question
+       let questionElement =document.getElementById("question");
+       questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+       // show options 
+       let choices = quiz.getQuestionIndex().choices;
+       for (let i = 0; i < choices.length; i++) {
+           let choiceElement = document.getElementById("choice"
+           + i);
+           choiceElement.innerHTML = choices[i];
+           guess("btn" + i, choices[i]);
+       }
+
+       showProgress();
+    }
+};
+
+// GUESS FUNCTION 
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        displayQuestion();
+    }
+}
+
+// show quiz progress
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let progressElement = document.getElementById("progress");
+    progressElement.innerHTML = 
+    `Question ${currentQuestionNumber} of ${quiz.question.length}
+    `;
+}
+
+// SHOW SCORE
+function showScores() {
+    let quizEndHTML =
+        `
+            <h1>passed the quiz</h1>
+            <h2 id="score">You Scored: ${quiz.score} of ${quiz.
+                question.length}</h2>
+            <div class="quiz-repeat">
+                 <a href="index.html">play again</a>   
+            </div>
+        `;
+}
